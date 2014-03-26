394354e59a180ed88b7d05334bbcf59c3641ae10
Jordan Nemrow <nemrowj@gmail.com>
2014-03-26 09:37:07 -0700

    Dynamic schedule with segmented layout functional

--- js/controller.js
+++ js/controller.js
@@ -15,15 +15,38 @@ var Controller = {
       success: Controller.loadUserDataSuccess,
       dataType: 'json'
     });
-    Controller.loadSchedule();
+  },
+
+  segmentedButtonsConfig: function () {
+    var labels = []
+    for (i=0; i < Model.schedule.length; i++) {
+      labels.push(Model.schedule[i].day_of_week);
+    }
+    var segmentedOptions = {
+      labels : labels,
+      selected: 1
+    };
+    return segmentedOptions;
   },
 
   loadUserDataSuccess: function (data) {
-    console.log(data)
+    Model.schedule = data.schedule;
+    Controller.loadSchedule();
+  },
+
+  activateSchedule: function () {
+    var segmentedOptions = {
+      labels : Controller.segmentedButtonsConfig().labels,
+      selected: Controller.segmentedButtonsConfig().selected
+    };
+    var newSegmented = $.UICreateSegmented(segmentedOptions);
+    $('#segmentedPanel').append(newSegmented);
+    $('.segmented').UIPanelToggle('#toggle-panels',function(){$.noop;});
   },
 
   loadSchedule: function () {
-    var context = {eventData: [1,2,3]};
+    var context = {schedule: Model.schedule};
     View.schedulePageContainer.append(Template.indiEvent(context));
+    Controller.activateSchedule();
   }
 };
