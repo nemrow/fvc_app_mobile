e8d9ba585e494fbeea8e58f954bbc36d5e22f398
Jordan Nemrow <nemrowj@gmail.com>
2014-03-25 22:06:29 -0700

    Initial Api Implementation

--- js/controller.js
+++ js/controller.js
@@ -10,7 +10,16 @@ var Controller = {
   },
 
   loadUserData: function () {
+    $.ajax({
+      url: "http://localhost:3000/api/v1/load_data?login_code=1234",
+      success: Controller.loadUserDataSuccess,
+      dataType: 'json'
+    });
     Controller.loadSchedule();
+  },
+
+  loadUserDataSuccess: function (data) {
+    console.log(data)
   },
 
   loadSchedule: function () {
